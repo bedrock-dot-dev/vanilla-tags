5a233da69c092b11e447f6923ac5b016315bf7ba
destruc7i0n <destruc7i0n@users.noreply.github.com>
2026-03-22 23:16:54 -0700

    use latest server version

--- scripts/setup_bds.sh
+++ scripts/setup_bds.sh
@@ -21,10 +21,13 @@ chmod +x bds/bedrock_server
 HEADER_UUID=$(uuidgen | tr '[:upper:]' '[:lower:]')
 MODULE_UUID=$(uuidgen | tr '[:upper:]' '[:lower:]')
 
+SCRIPT_VERSION=$(curl -s https://registry.npmjs.org/@minecraft/server/latest | jq -r '.version')
+echo "Using @minecraft/server@$SCRIPT_VERSION"
+
 # copy pack files
 mkdir -p bds/behavior_packs/vanilla-tags/scripts
-jq --arg h "$HEADER_UUID" --arg m "$MODULE_UUID" \
-  '.header.uuid = $h | .modules[0].uuid = $m' \
+jq --arg h "$HEADER_UUID" --arg m "$MODULE_UUID" --arg v "$SCRIPT_VERSION" \
+  '.header.uuid = $h | .modules[0].uuid = $m | .dependencies[0].version = $v' \
   scripts/bp/manifest.json > bds/behavior_packs/vanilla-tags/manifest.json
 cp scripts/bp/scripts/main.js bds/behavior_packs/vanilla-tags/scripts/
 
